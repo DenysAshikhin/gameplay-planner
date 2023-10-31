5ee66711431e837eed72327c68c3d88c5c78c391
Denys Ashikhin <denysashikhin@gmail.com>
2023-10-31 14:01:41 -0400

    header, navbar, landing page and selector page finished porting

--- src/app/page_selection/page.js
+++ src/app/page_selection/page.js
@@ -1,9 +1,56 @@
+"use client"
+
+import { useEffect } from 'react';
+import ReactGA from "react-ga4";
+import PageCard from './page_card.jsx';
+
 
 export default function PageSelection() {
 
+    useEffect(() => {
+        let timeout = setTimeout(() => {
+
+            ReactGA.send({ hitType: "pageview", page: "/page_selection", title: "Page Selection" });
+        }, 5000);
+        return () => { clearTimeout(timeout) };
+
+
+    }, []);
+
     return (
-        <div>
-            Page Selection
+        <div
+            style={{
+                display: 'flex',
+                flex: '1',
+                backgroundColor: 'black',
+                position: 'relative',
+            }}
+        >
+            <div style={{
+                paddingLeft: '6px',
+                display: 'flex',
+                flexDirection: 'column',
+                flex: '1',
+                justifyContent: 'center',
+                backgroundColor: 'rgba(255,255,255, 0.08)',
+                paddingLeft: '60px'
+            }}>
+                <div
+                    style={{ display: 'flex' }}
+                >
+
+                    <PageCard page='upload' />
+                    <PageCard page='expedition' />
+                    <PageCard page='pets' />
+                </div>
+                <div
+                    style={{ display: 'flex', marginTop: '36px' }}
+                >
+                    <PageCard page='farm' />
+                    <PageCard page='cards' />
+                    <PageCard page='protein' />
+                </div>
+            </div>
         </div>
-    )
+    );
 }
---
+++ src/app/page_selection/page_card.jsx
@@ -0,0 +1,127 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const PageCard = ({ page, setTab }) => {
+  let imgString = "";
+  let nameString = "";
+  let url = "";
+  let imgSrc;
+  let tabNum = 0;
+
+  switch (page) {
+    case "upload":
+      imgString = `file_upload`;
+      nameString = `Upload`;
+      url = "/";
+      break;
+    case "expedition":
+      imgString = `signpost`;
+      nameString = `Expedition`;
+      tabNum = 1;
+      url = "/expeditions";
+      break;
+    case "pets":
+      imgString = `paw_plus`;
+      nameString = `Pets (Combos)`;
+      tabNum = 2;
+      url = "/pets";
+      break;
+    case "farm":
+      imgString = `farming`;
+      nameString = `Farm`;
+      tabNum = 3;
+      url = "/farming";
+      break;
+    case "cards":
+      imgString = `badge`;
+      nameString = `Cards`;
+      tabNum = 4;
+      url = "/cards";
+      break;
+    case "protein":
+      imgString = `gear_lightgray`;
+      //   imgSrc = gearIcon;
+      nameString = `Protein`;
+      tabNum = 7;
+      url = "/protein";
+      break;
+
+    default:
+      imgString = `file_upload`;
+      nameString = `Upload`;
+      break;
+  }
+  const disabled = false;
+  return (
+    <Link href={url} style={{ textDecoration: 'none' }}>
+      <div
+        className={"hover"}
+        style={{
+          height: "148px",
+          width: "150px",
+          marginRight: "48px",
+          borderRadius: "6px",
+          pointerEvents: disabled ? "none" : "",
+          opacity: disabled ? "0.3" : "",
+        }}
+      >
+        <div
+          style={{
+            height: "80%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "rgba(255,255,255, 0.05)",
+            borderTopRightRadius: "12px",
+            borderTopLeftRadius: "12px",
+            padding: "6px 6px 6px 6px",
+            borderTop: "2px solid rgba(255,255,255,0.8)",
+            borderLeft: "2px solid rgba(255,255,255,0.8)",
+            borderRight: "2px solid rgba(255,255,255,0.8)",
+          }}
+        >
+          <div
+            style={{
+              position: "relative",
+              height: "calc(100% - 12px)",
+              width: "calc(100% - 12px)",
+            }}
+          >
+            <Image
+              alt={`navigation item, picture of ${imgString}`}
+              src={imgSrc ? imgSrc : `/images/icons/${imgString}.svg`}
+              fill
+            />
+          </div>
+        </div>
+
+        <div
+          style={{
+            height: "20%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "rgba(255,255,255, 0.12)",
+            borderBottomRightRadius: "12px",
+            borderBottomLeftRadius: "12px",
+            borderBottom: "2px solid rgba(255,255,255,0.8)",
+            borderLeft: "2px solid rgba(255,255,255,0.8)",
+            borderRight: "2px solid rgba(255,255,255,0.8)",
+          }}
+        >
+          <div
+            className="importantText"
+            style={{
+              // marginTop: '6px',
+              fontSize: "20px",
+            }}
+          >
+            {nameString}
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default PageCard;
