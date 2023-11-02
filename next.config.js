29601b71b7328534de8d5add8637119e581de0d9
Denys Ashikhin <denysashikhin@gmail.com>
2023-11-02 09:37:23 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+          {
+            // This works, and returns appropriate Response headers:
+            source: '/(.*).png',
+            headers: [
+              {
+                key: 'Cache-Control',
+                value:
+                  'public, max-age=180, s-maxage=180, stale-while-revalidate=180',
+              },
+            ],
+          },
+          {
+            // This doesn't work for 'Cache-Control' key (works for others though):
+            source: '/_next/image(.*)',
+            headers: [
+              {
+                key: 'Cache-Control',
+                // Instead of this value:
+                value: 'public, max-age=180, s-maxage=180, stale-while-revalidate=180',
+                // Cache-Control response header is `public, max-age=60` in production
+                // and `public, max-age=0, must-revalidate` in development
+              },
+            ],
+          },
+        ]
+      },
 }
 
 module.exports = nextConfig
