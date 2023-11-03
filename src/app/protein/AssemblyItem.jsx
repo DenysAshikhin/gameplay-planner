83c9cb8cb5e952e57733a21fad360a8ffdd69fd3
Denys Ashikhin <denysashikhin@gmail.com>
2023-11-03 11:56:52 -0400

    added protein page

---
+++ src/app/protein/AssemblyItem.jsx
@@ -0,0 +1,103 @@
+import { useState, useEffect } from 'react';
+
+import useLocalStorage from "use-local-storage";
+import ReactGA from "react-ga4";
+import Image from 'next/image';
+
+const AssemblyItem = ({ e, currentWeights, setCurrentWeights }) => {
+
+    const [customWeight, setCustomWeight] = useLocalStorage(`assemblyBonusWeight-${e.id}`, -1);
+
+    useEffect(() => {
+        setCurrentWeights((curWeight) => {
+            if (!curWeight[e.id]) {
+                let temp = { ...curWeight };
+                temp[e.id] = customWeight === -1 ? e.defaultWeight : customWeight;
+                return temp;
+            }
+            else if (curWeight[e.id] !== (customWeight === -1 ? e.defaultWeight : customWeight)) {
+                let temp = { ...curWeight };
+                temp[e.id] = customWeight === -1 ? e.defaultWeight : customWeight;
+                return temp;
+            }
+            return curWeight;
+        })
+    }, [e, customWeight, setCurrentWeights])
+
+    return (
+        <>
+            <div style={{ width: '352px', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative' }}>
+
+                <div style={{ height: '95%', width: '90%', position: 'absolute' }}>
+                    <Image
+                        alt={`in game ${e.label} bonus icon`}
+                        src={e.img}
+                        fill
+                        unoptimized
+                    />
+                </div>
+
+                {/* <img 
+                alt={`in game ${e.label} bonus icon`}
+                    src={e.img}
+                    style={{
+                        width: '95%',
+                        //  height: '90%',
+                        position: 'absolute'
+                    }} /> */}
+
+                <div className='textMedium blackTextStroke1' style={{
+                    marginTop: '0', marginRight: '-32px', position: 'absolute', color: `rgba(255,255,255,0.9)`,
+                    fontWeight: 'bold',
+                    fontSize: '24px'
+                }}>
+                    {e.label}
+                </div>
+            </div>
+            <div style={{
+                width: '83px', display: 'flex', alignItems: 'center', justifyContent: 'center', boxSizing: 'border-box',
+                borderRight: '1px solid rgba(255,255,255,0.8)', borderLeft: '1px solid rgba(255,255,255,0.8)'
+            }}>
+                <div className='importantText textMedium2'>
+                    {e.defaultWeight}
+                </div>
+            </div>
+            <div style={{ display: 'flex', flex: '1', alignItems: 'center', justifyContent: 'center', padding: '2px 0' }}>
+
+                <input
+                    className='importantText textMedium2'
+                    style={{ borderRadius: '4px', width: '55%', height: '65%', backgroundColor: e.index % 2 === 0 ? '#2D2D2D' : '#353535' }}
+                    type='number' value={customWeight === -1 ? e.defaultWeight : customWeight}
+                    onChange={
+                        (inner_e) => {
+                            try {
+                                let x = Number(inner_e.target.value);
+                                // x = Math.floor(x);
+                                if (x < 0.00 || x > 99999) {
+                                    return;
+                                }
+                                setCustomWeight(x);
+
+                                ReactGA.event({
+                                    category: "protein_interaction",
+                                    action: `changed_assembly_bonus_weight`,
+                                    label: `${e.label}`,
+                                    value: x
+                                })
+
+                            }
+                            catch (err) {
+                                console.log(err);
+                            }
+                        }}
+                    min="0"
+                    max="99999"
+                />
+
+            </div>
+        </>
+    )
+}
+
+
+export default AssemblyItem;
---
+++ src/app/protein/AssemblyLine.jsx
@@ -0,0 +1,184 @@
+import { useState, useEffect } from 'react';
+
+import { BonusMap } from '../util/itemMapping.js';
+import farmingHelper from '../util/farmingHelper.js';
+import mathHelper from '../util/math.js';
+import helper from '../util/helper.js';
+import useLocalStorage from "use-local-storage";
+
+import rightArrow from '../../../public/images/icons/right_arrow_white.svg';
+import lockedAssembly from '../../../public/images/farming/assembly/Not_Unlocked.png';
+
+import Image from 'next/image';
+
+//AssemblyCostReductionBonus
+//AssemblerCollection
+
+//Need to manually check if unlocked or not later
+
+const AssemblyInnerBonus = ({ line, al_level, key }) => {
+
+    const [showLocked, setShowLocked] = useState(false);
+
+    let data = BonusMap[line.ID];
+    let bonusAmount = farmingHelper.calcAssemblyLine(line, al_level);
+    let futureBonusAmount = farmingHelper.calcAssemblyLine(line, al_level + 1);
+    let locked = false;
+    if ((al_level + 1) < line.StartingLevel) {
+        locked = true;
+    }
+    return (
+        <div
+            onMouseEnter={(e) => { setShowLocked(true) }}
+            onMouseLeave={(e) => { setShowLocked(false) }}
+            key={key}
+            style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative', width: '100%', height: '100%' }}>
+            {/* <div style={{ height: '95%', width: '90%', position: 'relative' }}> */}
+            <Image
+                alt={`in game ${data.label} assembly line image`}
+                src={!locked || showLocked ? data.img : lockedAssembly}
+                style={{ height: 'auto', width: '95%' }}
+                // fill
+                unoptimized
+            />
+            {/* </div> */}
+
+
+            {/* <img alt={`in game ${data.label} assembly line image`} src={!locked || showLocked ? data.img : lockedAssembly} style={{ width: '95%' }} /> */}
+
+
+            {(!locked || showLocked) && (
+                <div
+                    className='textMedium blackTextStroke1'
+                    // className='textMedium' 
+                    style={{
+                        left: '14.5%',
+                        width: '82%',
+                        position: 'absolute', color: `rgba(255,255,255,0.9)`,
+                        fontWeight: 'bold',
+                        fontSize: '30px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <div style={{
+                        marginRight: '12px',
+                        overflow: 'hidden',
+                        whiteSpace: 'nowrap',
+                        textOverflow: 'ellipsis'
+                    }}
+                    >
+                        {data.label}
+                    </div>
+                    <div style={{ fontSize: '24px', marginLeft: 'auto', marginRight: '6px', marginTop: '0px' }}>
+                        {`x` + bonusAmount.toExponential(2)}
+                    </div>
+
+                    <div style={{ height: '32px', width: '32px', position: 'relative', margin: '0 -3px' }}>
+                        <Image
+                            alt='arrow point to the left'
+                            src={rightArrow}
+                            fill
+                            unoptimized
+                        />
+                    </div>
+
+
+                    {/* <img alt='arrow point to the left' src={rightArrow} style={{ width: '32px', margin: '0 -3px' }} /> */}
+
+
+                    <div style={{ fontSize: '24px', marginLeft: '6px', marginTop: '0px' }}>
+                        {`x` + futureBonusAmount.toExponential(2)}
+                    </div>
+                </div>
+            )}
+
+        </div>
+    )
+}
+
+
+
+
+
+const AssemblyLine = ({ data, assemblyID, index, purchaseTime, cost }) => {
+
+
+    let assembly = data.AssemblerCollection[assemblyID];
+    if (!assembly)
+        return null;
+
+
+
+    let stringTimeToPurchase = helper.secondsToString(purchaseTime);
+
+    return (
+        <div
+            style={{
+                backgroundColor: 'rgba(255,255,255, 0.12)',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                marginTop: index > 1 ? '24px' : '3px',
+                marginRight: '3px',
+                borderRadius: '6px'
+            }}
+        >
+
+            <div className='importantText'
+                style={{
+                    backgroundColor: 'rgba(255,255,255, 0.12)',
+                    width: '100%',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    fontSize: '24px',
+                    borderRadius: '6px'
+                }}
+            >
+                <div style={{ marginLeft: '6px' }}>
+                    Purchase #{index}: Assembly {assemblyID + 1}
+                </div>
+                <div style={{ marginRight: '6px' }}>
+                    Level: {assembly.Level}
+                </div>
+            </div>
+
+
+            <div
+                style={{
+                    marginTop: '12px'
+                }}
+            >
+                {assembly.BonusList.map((e, inner_index) => {
+                    return <AssemblyInnerBonus key={inner_index} line={e} al_level={assembly.Level} />
+                })}
+                <div style={{ marginBottom: '6px' }}></div>
+            </div>
+
+            <div className='importantText'
+                style={{
+                    backgroundColor: 'rgba(255,255,255, 0.12)',
+                    width: '100%',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    fontSize: '24px',
+                    borderRadius: '6px'
+                }}
+            >
+                <div style={{ marginLeft: '6px' }}>
+                    Cost: {cost.toExponential(2).toString()}
+                </div>
+                <div style={{ marginRight: '6px' }}>
+                    Time to Purchase: {stringTimeToPurchase}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default AssemblyLine;
