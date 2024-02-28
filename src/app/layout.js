8167eff1f544e0d2cc6e9cb1c2cf819b25e3893d
Denys Ashikhin <denysashikhin@gmail.com>
2024-02-28 12:01:23 -0500

    fixing some crawling issues

--- src/app/layout.js
+++ src/app/layout.js
@@ -47,7 +47,8 @@ export default function RootLayout({ children }) {
       //   __html: `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1393057374484862" crossorigin="anonymous"></script>`
       // }}
       >
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1393057374484862" crossOrigin="anonymous"></script>
+        <meta name="robots" content="all" />
+        {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1393057374484862" crossOrigin="anonymous"></script> */}
       </head>
       <body className=''>
         <div
