e4fa92f3b2425f2a0d533bee95c09460bcce6678
Denys Ashikhin <denysashikhin@gmail.com>
2024-02-29 22:52:22 -0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -53,7 +53,13 @@ export default function RootLayout({ children }) {
         
         <div
             dangerouslySetInnerHTML={{
-              __html: `<script>    (function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "l9ob1f8705");</script>`,
+              __html: `<script type="text/javascript">
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "l9ob1f8705");
+          </script>`,
             }}
           ></div>
 
