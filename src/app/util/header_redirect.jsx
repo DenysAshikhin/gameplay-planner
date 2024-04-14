32020e3e71a19431cc834dd7ea11b936cfec5678
Denys Ashikhin <denysashikhin@gmail.com>
2024-04-14 14:20:03 -0400

    Update header_redirect.jsx

--- src/app/util/header_redirect.jsx
+++ src/app/util/header_redirect.jsx
@@ -1,13 +1,13 @@
 "use client"
 
-export default () => {
+export default function () {
 
     return <div
-    className="hover"
-    style={{position:'absolute', top:'0', left:'0', height:"36px", width:'100vw', zIndex:'2'}}
-    onClick={() => {
-        window.location.href = 'https://www.gameplayplanner.com/page_selection'
-    }}>
+        className="hover"
+        style={{ position: 'absolute', top: '0', left: '0', height: "36px", width: '100vw', zIndex: '2' }}
+        onClick={() => {
+            window.location.href = 'https://www.gameplayplanner.com/page_selection'
+        }}>
 
     </div>
 }
