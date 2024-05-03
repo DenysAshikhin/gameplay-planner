964ecb9cb5d7e3891615d8c428907049e693e8f0
Denys Ashikhin <denysashikhin@gmail.com>
2024-05-03 12:32:18 -0400

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,6 +36,8 @@ export default function RootLayout({ children }) {
       //   __html: `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1393057374484862" crossorigin="anonymous"></script>`
       // }}
       >
+
+        <meta name="google-adsense-account" content="ca-pub-1393057374484862" />
         <meta name="yandex-verification" content="347889c423938e18" />
         <meta name="robots" content="all" />
         <Clarity />
